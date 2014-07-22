63c64541354614747c1fe68e82a6eb9ffdd3259f
Maciej Skierkowski <maciej@skierkowski.com>
2014-07-22 11:08:28 -0700

    Shorter descriptions

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,9 +11,9 @@ program :description, 'Factor.io Server to run workflows'
 command 'server' do |c|
   c.syntax = 'factor server [options]'
   c.description = 'Start the Factor.io Server in the current local directory'
-  c.option '--log FILE', String, 'File location of where to log output. Default is stdout.'
-  c.option '--credentials FILE', String, 'File location of credentials.yml'
-  c.option '--connectors FILE', String, 'File location of connectors.yml'
+  c.option '--log FILE', String, 'Log file path. Default is stdout.'
+  c.option '--credentials FILE', String, 'credentials.yml file path.'
+  c.option '--connectors FILE', String, 'connectors.yml file path'
   c.option '--path FILE', String, 'Path to workflows'
   c.when_called Factor::Commands::Workflow, :server
 end
