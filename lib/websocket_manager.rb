5324af7eb9d79bc7c37c8b4de6a7f73f55d33b98
Maciej Skierkowski <maciej@skierkowski.com>
2014-07-24 20:41:53 -0700

    wrap in 'Factor' module

--- lib/websocket_manager.rb
+++ lib/websocket_manager.rb
@@ -3,92 +3,93 @@
 require 'faye/websocket'
 require 'uri'
 
-# class for managing the web socket connections
-class WebSocketManager
-  attr_accessor :keep_open, :events, :state
+module Factor
+  # class for managing the web socket connections
+  class WebSocketManager
+    attr_accessor :keep_open, :events, :state
 
-  def initialize(uri, headers = {})
-    u                     = URI(uri)
-    u.scheme              = 'wss' if u.scheme == 'https'
-    @uri                  = u.to_s
-    @settings             = { ping: 10, retry: 5 }
-    @settings[:headers]   = headers if headers && headers != {}
-    @state                = :closed
-    @events               = {}
-  end
-
-  def open
-    if closed?
-      @state = :opening
-      connect
+    def initialize(uri, headers = {})
+      u                     = URI(uri)
+      u.scheme              = 'wss' if u.scheme == 'https'
+      @uri                  = u.to_s
+      @settings             = { ping: 10, retry: 5 }
+      @settings[:headers]   = headers if headers && headers != {}
+      @state                = :closed
+      @events               = {}
     end
-    @state
-  end
 
-  def close
-    if open?
-      @state = :closing
-      @ws.close
+    def open
+      if closed?
+        @state = :opening
+        connect
+      end
+      @state
     end
-    @state
-  end
 
-  def on(event, &block)
-    @events[event] = block
-  end
+    def close
+      if open?
+        @state = :closing
+        @ws.close
+      end
+      @state
+    end
 
-  def open?
-    @state == :open
-  end
+    def on(event, &block)
+      @events[event] = block
+    end
 
-  def opening?
-    @state == :opening
-  end
+    def open?
+      @state == :open
+    end
 
-  def closed?
-    @state == :closed
-  end
+    def opening?
+      @state == :opening
+    end
 
-  def closing?
-    @state == :closing
-  end
+    def closed?
+      @state == :closed
+    end
 
-  def send(msg)
-    @ws.send(msg)
-  end
+    def closing?
+      @state == :closing
+    end
 
-  private
+    def send(msg)
+      @ws.send(msg)
+    end
 
-  def call_event(event, data)
-    @events[event].call(data) if @events[event]
-  end
+    private
 
-  def connect
-    EM.run do
-      begin
-        @ws = Faye::WebSocket::Client.new(@uri, nil, @settings)
+    def call_event(event, data)
+      @events[event].call(data) if @events[event]
+    end
 
-        @ws.on :close do |event|
-          @state = :closed
-          call_event :close, event
+    def connect
+      EM.run do
+        begin
+          @ws = Faye::WebSocket::Client.new(@uri, nil, @settings)
+
+          @ws.on :close do |event|
+            @state = :closed
+            call_event :close, event
+          end
+
+          @ws.on :message do |msg|
+            call_event :message, msg
+          end
+
+          @ws.on :open do |event|
+            @state = :open
+            call_event :open, event
+          end
+
+          @ws.on :error do |event|
+            call_event :error, event
+          end
+        rescue => ex
+          call_event :fail, ex.message
         end
-
-        @ws.on :message do |msg|
-          call_event :message, msg
-        end
-
-        @ws.on :open do |event|
-          @state = :open
-          call_event :open, event
-        end
-
-        @ws.on :error do |event|
-          call_event :error, event
-        end
-      rescue => ex
-        call_event :fail, ex.message
       end
     end
   end
-
 end
