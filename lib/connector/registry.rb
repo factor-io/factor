449d7b8491dddb0fbc6eaaf401dea39aab7d48a3
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-07 14:54:39 -0700

    Creating a registry

--- lib/connector/registry.rb
+++ lib/connector/registry.rb
@@ -1,6 +1,18 @@
 module Factor
   module Connector
-    class Registry
+    module Registry
+
+      def self.get(id)
+        get_class = self.class.constants.find do |class_name|
+          begin
+            class_obj = self.class.const_get(class_name)
+            class_obj.superclass == Factor::Connector::Definition && class_obj.new.id == id
+          rescue
+          end
+        end
+        raise "No definition found with id #{id}" unless get_class
+        self.class.const_get(get_class).new
+      end
     end
   end
 end
---
+++ spec/connectors/registry_spec.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+require 'connector/definition'
+require 'connector/registry'
+
+describe Factor::Connector::Registry do
+  before do
+    class MyDef < Factor::Connector::Definition
+      id :my_def
+    end
+
+  end
+
+  it 'can load definition by ID' do
+    definition = Factor::Connector::Registry.get(:my_def)
+    expect(definition).to be_a(MyDef)
+    expect(definition.class.superclass).to eq(Factor::Connector::Definition)
+  end
+
+  it 'fails on bad ID' do
+    expect {
+      Factor::Connector::Registry.get(:foo)
+    }.to raise_error
+  end
+
+end
