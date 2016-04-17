180966821bb54ced892ddff2f102dc892a1cc47c
Maciej Skierkowski <maciej@skierkowski.com>
2016-04-17 09:32:28 -0700

    Test reset

---
+++ spec/commands/run_spec.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+require 'factor/commands/run_command'
+
+describe Factor::Commands::RunCommand do
+  describe 'run' do
+    it 'can run a basic workflow' do
+
+    end
+  end
+end
---
+++ spec/logger_spec.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+require 'factor/logger'
+
+describe Factor::Logger do
+  describe 'run' do
+    it 'can run a basic workflow' do
+
+    end
+  end
+end
