e117a62cf55cd8d3cc71467fec7099098b16c97e
Maciej Skierkowski <maciej@skierkowski.com>
2016-05-07 23:14:03 -0700

    register and get test for Connector

--- spec/connector_spec.rb
+++ spec/connector_spec.rb
@@ -5,7 +5,15 @@ require 'spec_helper'
 require 'factor/commands/run_command'
 
 describe Factor::Connector do
-  it 'can' do
 
+  it 'can register and retreive a class' do
+    class Sample < Factor::Connector
+    end
+
+    registered = Factor::Connector.register(Sample)
+    found_class = Factor::Connector.get('sample')
+
+    expect(registered).to eq(Sample)
+    expect(found_class).to be(Sample)
   end
 end
