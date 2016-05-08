c444bd6ba9a30e334e36c3d7bcccd890a195ec03
Maciej Skierkowski <maciej@skierkowski.com>
2016-05-07 22:50:07 -0700

    Test for logs

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -5,9 +5,19 @@ require 'spec_helper'
 require 'factor/logger'
 
 describe Factor::Logger do
-  describe 'run' do
-    it 'can run a basic workflow' do
+  before :all do
+    @logger = Factor::Logger.new
+  end
+  
+  it 'logs with coloring' do
+    expect { @logger.info('test')    }.to output(/\[37m.*\[0m/).to_stdout
+    expect { @logger.success('test') }.to output(/\[32m.*\[0m/).to_stdout
+    expect { @logger.warn('test')    }.to output(/\[33m.*\[0m/).to_stdout
+    expect { @logger.error('test')   }.to output(/\[31m.*\[0m/).to_stdout
+    expect { @logger.debug('test')   }.to output(/\[38;5;59m.*\[0m/).to_stdout
+  end
 
-    end
+  it 'logs with the right format' do
+    expect { @logger.info('test')}.to output(/\[[0-9]{2}\/[0-9]{2}\/[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\]\s.*test\e\[0m\n/).to_stdout
   end
 end
