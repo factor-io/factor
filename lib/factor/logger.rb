aa8b88652bbbd6f1a8fb8d929c2d79476516243c
Maciej Skierkowski <maciej@skierkowski.com>
2016-05-07 22:19:37 -0700

    Adding API docs to Factor::Logger

--- lib/factor/logger.rb
+++ lib/factor/logger.rb
@@ -1,6 +1,7 @@
 require 'rainbow'
 
 module Factor
+  # Default text logger used by Factor. Displays logs with timestamps, indentation, and coloring
   class Logger
     attr_accessor :indentation
     
@@ -8,32 +9,50 @@ module Factor
       @indentation = 0
     end
 
+    # Given an indentation level all messages logged inside of the provided block
+    # will be indented to the defined depth.
+    # @param indentation [Integer] the depth to indent
     def indent(indentation=1, &block)
       @indentation += indentation
       block.call
       @indentation -= indentation
     end
 
+    # Logs a string message to standard output using the format `[<time>] <message>` with
+    # appropriate coloring (:debug => grey, :error => red, :info => white, :warn=> yellow
+    # :success => green) and indentation
+    # @param type [Symbol] type of message to log (:debug, :info, :warn, :error, :success)
+    # @param message [String] message to log
     def log(log_level, message)
       puts "[#{time}] #{'  ' * @indentation}#{color(log_level, message)}"
     end
 
+    # Logs a debug message to standard output with formatting
+    # @param message [String] message to log
     def debug(message)
       log :debug, message
     end
 
+    # Logs a info message to standard output with formatting
+    # @param message [String] message to log
     def info(message)
       log :info, message
     end
 
+    # Logs a warn message to standard output with formatting
+    # @param message [String] message to log
     def warn(message)
       log :warn, message
     end
 
+    # Logs a error message to standard output with formatting
+    # @param message [String] message to log
     def error(message)
       log :error, message
     end 
 
+    # Logs a success message to standard output with formatting
+    # @param message [String] message to log
     def success(message)
       log :success, message
     end
