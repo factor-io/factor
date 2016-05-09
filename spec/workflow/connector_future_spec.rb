697b38d7d0c77aae05c59d933f2160699ab83a57
Maciej Skierkowski <maciej@skierkowski.com>
2016-05-08 22:21:40 -0700

    trigger and subscribe

--- spec/workflow/connector_future_spec.rb
+++ spec/workflow/connector_future_spec.rb
@@ -3,11 +3,46 @@
 require 'spec_helper'
 
 require 'factor/workflow/connector_future'
+require 'factor/connector'
 
 describe Factor::Workflow::ConnectorFuture do
-  describe 'run' do
-    it 'can run a basic workflow' do
-
+  it 'can handle triggers' do
+    logger = double('logger', log:true, trigger:true)
+    class Sample < Factor::Connector
+      def run
+        trigger this:'is useful'
+      end
     end
+
+    connector        = Sample.new
+    connector_future = Factor::Workflow::ConnectorFuture.new(connector)
+
+    connector_future.on(:trigger) do |data|
+      logger.trigger(data)
+    end
+
+    expect(logger).to receive(:trigger).with(this:'is useful')
+
+    connector_future.wait
+  end
+
+  it 'can handle logs' do
+    logger = double('logger', info: true)
+    class Sample < Factor::Connector
+      def run
+        info 'Hi'
+      end
+    end
+
+    connector        = Sample.new
+    connector_future = Factor::Workflow::ConnectorFuture.new(connector)
+
+    connector_future.on(:info) do |data|
+      logger.info(data)
+    end
+
+    expect(logger).to receive(:info).with('Hi')
+
+    connector_future.wait
   end
 end
