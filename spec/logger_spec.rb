8adff9214b834360cdca80f8bccb64768c22871b
Maciej Skierkowski <maciej@skierkowski.com>
2016-05-08 21:09:23 -0700

    Adding test for indentation and unrecognized types

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -17,7 +17,19 @@ describe Factor::Logger do
     expect { @logger.debug('test')   }.to output(/\[38;5;59m.*\[0m/).to_stdout
   end
 
+  it 'logs even when type not recognized' do
+    expect { @logger.log(:not_here, 'test') }.to output(/test/).to_stdout
+  end
+
   it 'logs with the right format' do
     expect { @logger.info('test')}.to output(/\[[0-9]{2}\/[0-9]{2}\/[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\]\s.*test\e\[0m\n/).to_stdout
+  end
+
+  it 'logs with indentation' do
+    expect do
+      @logger.indent 2 do
+        @logger.info('test')
+      end
+    end.to output(/\s{4}\e\[37m.*\[0m/).to_stdout
   end
 end
