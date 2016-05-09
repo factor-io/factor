015e8e6711f4de6c781fcc4b6536647d2254dc2a
Maciej Skierkowski <maciej@skierkowski.com>
2016-05-08 20:59:11 -0700

    Basic test on run command

--- spec/commands/run_spec.rb
+++ spec/commands/run_spec.rb
@@ -3,11 +3,33 @@
 require 'spec_helper'
 
 require 'factor/commands/run_command'
+require 'factor/connector'
+require 'ostruct'
 
 describe Factor::Commands::RunCommand do
-  describe 'run' do
-    it 'can run a basic workflow' do
+  it 'can run a connector from the commandline' do
+    logger = double('logger', log: true, indent: true)
 
+    class Sample < Factor::Connector
+      attr_accessor :test_param
+      def initialize(options={})
+        @test_param = options[:test]
+      end
+
+      def run
+        'Hello'
+      end
     end
+
+    Factor::Connector.register(Sample)
+
+    run_command = Factor::Commands::RunCommand.new(logger: logger)
+
+    expect(logger).to receive(:log).with(:info, /Using default '.*' settings file/)
+    expect(logger).to receive(:log).with(:warn, /Couldn\'t open the settings file '.*', continuing without settings/)
+    expect(logger).to receive(:log).with(:info, "Running 'sample({})'")
+    expect(logger).to receive(:log).with(:success, "Response:")
+
+    run_command.run(['sample'],OpenStruct.new(verbose: true))
   end
 end
