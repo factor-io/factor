d3dd738193e3df3d858a69f998bdfda5b95f2350
Maciej Skierkowski <maciej@skierkowski.com>
2016-05-08 21:34:49 -0700

    Adding coverage for logs and triggers

--- spec/connector_spec.rb
+++ spec/connector_spec.rb
@@ -16,4 +16,44 @@ describe Factor::Connector do
     expect(registered).to eq(Sample)
     expect(found_class).to be(Sample)
   end
+
+  it 'can handle logs' do
+    logger = double('logger', log:true, trigger:true)
+    class Sample < Factor::Connector
+      def run
+        debug 'debug'
+        info 'i talk a lot'
+        warn 'careful'
+        success 'good news'
+        error 'bad news'
+      end
+    end
+    
+    connector = Sample.new
+    connector.add_observer(logger, :trigger)
+
+    expect(logger).to receive(:trigger).with(:debug, 'debug')
+    expect(logger).to receive(:trigger).with(:info, 'i talk a lot')
+    expect(logger).to receive(:trigger).with(:warn, 'careful')
+    expect(logger).to receive(:trigger).with(:success, 'good news')
+    expect(logger).to receive(:trigger).with(:error, 'bad news')
+
+    connector.run
+  end
+
+  it 'can handle triggers' do
+    logger = double('logger', log:true, trigger:true)
+    class Sample < Factor::Connector
+      def run
+        trigger this:'is useful'
+      end
+    end
+
+    connector = Sample.new
+    connector.add_observer(logger, :trigger)
+
+    expect(logger).to receive(:trigger).with(:trigger, this:'is useful')
+
+    connector.run
+  end
 end
